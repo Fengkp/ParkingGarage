829c122cfa6478c4cdf42d34e2bb1150af7a7f17
Feng Parra <parrafk@gmail.com>
2017-12-11 12:01:33 -0500

    Edited OpenTicketViewController class.

--- src/garage/view/OpenTicketViewController.java
+++ src/garage/view/OpenTicketViewController.java
@@ -19,7 +19,7 @@ public class OpenTicketViewController extends UniversalComponents {
     private String lastName;
     private String licensePlate;
 
-    public void submitBtn(ActionEvent event) throws IOException, ClassNotFoundException {
+    public void submitBtn(ActionEvent event) throws IOException {
         if (firstNameText.getText().isEmpty()
                 || lastNameText.getText() .isEmpty() || licensePlateText.getText().isEmpty())
             super.errorBox("Missing Information", "Please fill in all the information.");
